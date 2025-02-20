7155ff4f0202e2283f7ad633402823e8ce641439
Deathstar606 <dsfardin606@gmail.com>
2025-02-20 16:46:10 +0600

    added galaxy renovation to family

--- src/components/Case Studies.js
+++ src/components/Case Studies.js
@@ -12,6 +12,7 @@ import ConFu from "../images/Case Logo/ConFusion.png";
 import Codex from "../images/Case Logo/16353_3867287_580791_image.jpg"
 import Digi from "../images/Case Logo/Digixwhitelogo.png"
 import Vergo from "../images/Case Logo/Vergo.png"
+import Gren from "../images/Case Logo/GALAXY_WEBSITE_Transparent.svg"
 import MediaQuery from "react-responsive";
 
 const SliderRev = {
@@ -180,6 +181,7 @@ function RenderItem() {
             <HovRev text="ConFusion" text2="Restaurant Application" src={ConFu} src1={Rect} src2={mongo} link={"https://deathstar606.github.io/ConFusion/#/home"}/>
             <HovRev text="GlassCo" text2="Eyewear Store App" src={Glass} src1={Rect} src2={mongo} link={"https://deathstar606.github.io/GlassCo/#/home"}/>
             <HovRev text="Digix" text2="Ongoing project" src={Digi} src1={Rect} src2={Json} link={"https://deathstar606.github.io/Digix/#/home"}/>
+            <HovRev text="Galaxy Renovations" text2="Renovation Corporation" src={Gren} src1={Rect} src2={mongo} link={" https://deathstar606.github.io/GalaxyRen/#/home"}/>
             </>                   
     );
 }
